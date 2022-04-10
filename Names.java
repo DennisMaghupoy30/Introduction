74c9a3182527514e8a34e2b7c556cf86b52d4281
unknown <s1920176@usls.edu.ph>
2022-04-10 09:37:33 +0800

    second commit

--- Names.java
+++ Names.java
@@ -1,5 +1,14 @@
 public class Names{
-	String GivenName;
-	String SurName;
-	
+		private String GivenName;
+		private String SurName;
+				public Person(String Given, String Sur){
+					GivenName=Given;
+					SurName=Sur;
+}
+public String GetGivenName(){
+	return GivenName;
+}
+public String GetSurName(){
+	return SurName;
+}
 }
