2e28f26753a80f02925dae8fa8dec1ea8df705a8
unknown <s1920176@usls.edu.ph>
2022-04-10 09:39:48 +0800

    Names

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(string[] args){
+		Person a = new Person ("Dennis" ,"Maghupoy \n");
+		Person b = new Person ("Miery" ,"Fernandez \n");
+		Person c = new Person ("Juliet" ,"Hinayan \n");
+		Person d = new Person ("Drexel" ,"Amit \n");
+		Person e = new Person ("kerguelen" ,"Montales \n");
+
+System.out.println(a.GetGivenName()+a.GetSurName());
+System.out.println(b.GetGivenName()+b.GetSurName());
+System.out.println(c.GetGivenName()+c.GetSurName());
+System.out.println(d.GetGivenName()+d.GetSurName());
+System.out.println(e.GetGivenName()+e.GetSurName());
+}}
